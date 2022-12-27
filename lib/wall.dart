522b33bc58e4b3052699315228721f41d334a89e
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-12-27 12:11:19 +0100

    world-bounds

--- lib/wall.dart
+++ lib/wall.dart
@@ -1,20 +1,6 @@
 import 'package:flame/palette.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 
-List<Wall> createBoundaries(Vector2 screenSize) {
-  final Vector2 topLeft = Vector2.zero();
-  final Vector2 bottomLeft = Vector2(0, screenSize.y + 0);
-  final Vector2 bottomRight = Vector2(screenSize.x, screenSize.y + 0);
-  final Vector2 topRight = Vector2(screenSize.x, 0);
-
-  return [
-    Wall(topLeft, topRight),
-    Wall(topRight, bottomRight),
-    Wall(bottomLeft, topLeft),
-    Wall(bottomRight, bottomLeft),
-  ];
-}
-
 class Wall extends BodyComponent {
   final Vector2 start;
   final Vector2 end;
