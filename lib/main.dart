50c876fbd2bb3c7d8afbced00764725b444c330f
Mahdi <mohammadmahdi.amini@nordnet.se>
2023-01-08 11:48:34 +0100

    making orientation work

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,14 @@ import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame_playground/my_game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeRight,
+    DeviceOrientation.landscapeLeft,
+  ]);
   await Flame.device.fullScreen();
   await Flame.device.setLandscape();
 
